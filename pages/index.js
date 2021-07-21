6a8e8012d0412cdaf6f619d504c2ca11164f6e5f
Rayan Kazi <rayan.quack.you@gmail.com>
2021-07-20 19:32:05 -0700

    created modal

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,56 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import NewDocumentSection from "../components/NewDocumentSection";
 import Icon from "@material-tailwind/react/Icon";
+import Button from "@material-tailwind/react/Button";
 import { getSession, useSession } from "next-auth/client";
 import Login from "../components/Login";
+import Modal from "@material-tailwind/react/Modal";
+import ModalBody from "@material-tailwind/react/ModalBody";
+import ModalFooter from "@material-tailwind/react/ModalFooter";
+import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [session] = useSession();
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState("");
 
   if (!session) return <Login />;
+
+  const createDocument = () => {
+    setShowModal(false);
+    setInput("");
+  };
+
+  const modal = (
+    <Modal size="sm" active={showModal} toggler={() => setShowModal(false)}>
+      <ModalBody>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="outline-none w-full"
+          placeholder="Enter the name of the document..."
+          onKeyDown={(e) => e.key === "Enter" && createDocument()}
+        />
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          color="blue"
+          buttonType="link"
+          onClick={(e) => setShowModal(false)}
+          ripple="dark"
+        >
+          Cancel
+        </Button>
+
+        <Button color="blue" onClick={createDocument} ripple="light">
+          Create
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 
   return (
     <div className="">
@@ -18,7 +60,38 @@ export default function Home() {
       </Head>
 
       <Header />
-      <NewDocumentSection />
+      {modal}
+
+      <section className="bg-[#f8f9fa] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+
+            <Button
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="3xl" />
+            </Button>
+          </div>
+
+          <div>
+            <div
+              className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700"
+              onClick={() => setShowModal(true)}
+            >
+              <Image src="https://links.papareact.com/pju" layout="fill" />
+            </div>
+
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">
+              Blank
+            </p>
+          </div>
+        </div>
+      </section>
 
       <section className="bg-white px-10 md:px-0">
         <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
